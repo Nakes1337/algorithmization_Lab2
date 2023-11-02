382e1a64b339fe1930ed149fdc37c052a39d6008
Nakes1337 <48197706+Nakes1337@users.noreply.github.com>
2023-11-02 15:17:52 +0300

    Add files via upload

---
+++ Lab2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ofstream out;
+    out.open("textfile.txt");
+    out << "Success !!!" << endl;
+    out.close();
+
+    ifstream in("textfile.txt");
+    string line;
+    while (getline(in, line)) cout << line << endl;
+
+    return 0;
+}
