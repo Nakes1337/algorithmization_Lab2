2111055f0d3a3001453b9f822d00dba295a559d3
Nakes1337 <48197706+Nakes1337@users.noreply.github.com>
2023-11-02 15:18:11 +0300

    Update Lab2.cpp

--- Lab2.cpp
+++ Lab2.cpp
@@ -1,19 +1,26 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <string>
 
 using namespace std;
 
-int main()
-{
-    ofstream out;
-    out.open("textfile.txt");
-    out << "Success !!!" << endl;
-    out.close();
+float autofunc(float x) {
+	float y = 0;
+	if (x < -4) y = -2;
+	else if (x < 0) y = x / 4;
+	else if (x < 2) y = x * x;
+	else y = (x - 10) / -2;
+	return y;
+}
 
-    ifstream in("textfile.txt");
-    string line;
-    while (getline(in, line)) cout << line << endl;
 
-    return 0;
+int main() {
+	ofstream out;
+	out.open("textfile.txt");
+	for (float x = -6; x <= 12; x++) out << "X:= " << x << "\tY:= " << autofunc(x) << endl;
+	out.close();
+	ifstream in("textfile.txt");
+	string line;
+	while (getline(in, line)) cout << line << endl;
+	return 0;
 }
